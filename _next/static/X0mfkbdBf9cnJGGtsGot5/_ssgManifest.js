dfb58fac7e580b1d06e476abf63a62cdc8247579
maxhuang8 <maxhuang8@users.noreply.github.com>
2025-06-06 23:42:28 +0000

    deploy: 0abecb85c852dd48085c9ec8032c32af18b6a3a2

---
+++ _next/static/X0mfkbdBf9cnJGGtsGot5/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F[slug]","\u002Fabout","\u002Fcontact","\u002Fprojects","\u002Fresearch","\u002Frunning","\u002Fwriting"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
